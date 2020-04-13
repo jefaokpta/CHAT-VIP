76f68f50f0dfe658f9a260ea3eae01d1bb2c913d
Jefferson Reis <jefaokpta@hotmail.com>
2020-04-13 15:11:36 -0300

    gritando na sala

--- js/js.js
+++ js/js.js
@@ -3,7 +3,7 @@ var room;
 params.forEach(p => {
     room = p;
 });
-console.log(room)
+console.log('SALA!!!!!  -  '+room)
 if(localStorage.getItem('vipRoom')){
     room = localStorage.getItem('vipRoom');
 }
